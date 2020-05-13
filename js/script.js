419654ef33a1ed19ada991dedfbdb92202ec02d7
ricardofagodoy <ricardofagodoy@gmail.com>
2020-05-13 19:21:44 -0300

    fixing vh

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ console.log(`Safe Investing Prototype (${new Date()})`)
 function move(page) {
     window.location.href = page + '.html'
 }
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01
+
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`)
+console.log(`For this screen, 1vh is ${vh}px`)
